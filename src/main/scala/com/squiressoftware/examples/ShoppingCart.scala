3c13aab05f28c46404d6a0322495b83fa7114de9
Thomas Squires <mjollnir8@gmail.com>
2017-06-19 18:54:40 +0200

    change cart to accept special offers on oranges and apples

--- src/main/scala/com/squiressoftware/examples/ShoppingCart.scala
+++ src/main/scala/com/squiressoftware/examples/ShoppingCart.scala
@@ -25,7 +25,28 @@ object ShoppingCart {
   }
 
   private def getBasketItemsCosts(basket: List[ShoppingItem.ShoppingItem]): BigDecimal = {
+    val apples = basket.filter(x => x.equals(ShoppingItem.Apple))
+    val oranges = basket.filter(x => x.equals(ShoppingItem.Orange))
+
+    if (isEleigleForApplesOffer(basket)) {
+      val remainingBasket = apples.take(apples.size - 2) ++ oranges
+      return ShoppingItem.getItemValue(ShoppingItem.Apple) + getBasketItemsCosts(remainingBasket)
+    }
+
+    if (isEleigleForOrangesOffer(basket)) {
+      val remainingBasket = apples ++ oranges.take(oranges.size - 3)
+      return (2 * ShoppingItem.getItemValue(ShoppingItem.Orange)) + getBasketItemsCosts(remainingBasket)
+    }
+
     return basket.map(i => ShoppingItem.getItemValue(i)).sum
+  }
+
+  private def isEleigleForApplesOffer(basket: List[ShoppingItem.ShoppingItem]): Boolean = {
+    basket.count(i => i.equals(ShoppingItem.Apple)) >= 2
+  }
+
+  private def isEleigleForOrangesOffer(basket: List[ShoppingItem.ShoppingItem]): Boolean = {
+    basket.count(i => i.equals(ShoppingItem.Orange)) >= 3
   }
 
 }
